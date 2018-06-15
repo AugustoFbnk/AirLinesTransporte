3cb049d24e76076a16867133b01bf9c07db3aaf6
AugustoFbnk <aferbonink@gmail.com>
2018-06-15 03:06:00 -0300

    Correção linhas ShowMessage

--- AirLinesTrasnporte/Program.cs
+++ AirLinesTrasnporte/Program.cs
@@ -26,45 +26,19 @@ namespace AirLinesTrasnporte
 
             toAviao.ShowMessages += ShowMessages;
             toTerminal.ShowMessages += ShowMessages;
-
             toAviao.RealizarTravessia(policial, bandido);
-            toAviao.Mensagens.ForEach(Console.WriteLine);
-
             toTerminal.RealizarTravessia(policial);
-            toTerminal.Mensagens.ForEach(Console.WriteLine);
-
             toAviao.RealizarTravessia(piloto, policial);
-            toAviao.Mensagens.ForEach(Console.WriteLine);
-
             toTerminal.RealizarTravessia(piloto);
-            toTerminal.Mensagens.ForEach(Console.WriteLine);
-
             toAviao.RealizarTravessia(piloto, oficialI);
-            toAviao.Mensagens.ForEach(Console.WriteLine);
-
             toTerminal.RealizarTravessia(piloto);
-            toTerminal.Mensagens.ForEach(Console.WriteLine);
-
             toAviao.RealizarTravessia(chefeDeServico, comissariaI);
-            toAviao.Mensagens.ForEach(Console.WriteLine);
-
             toTerminal.RealizarTravessia(chefeDeServico);
-            toTerminal.Mensagens.ForEach(Console.WriteLine);
-
             toAviao.RealizarTravessia(piloto, oficialII);
-            toAviao.Mensagens.ForEach(Console.WriteLine);
-
             toTerminal.RealizarTravessia(piloto);
-            toTerminal.Mensagens.ForEach(Console.WriteLine);
-
             toAviao.RealizarTravessia(chefeDeServico, comissariaII);
-            toAviao.Mensagens.ForEach(Console.WriteLine);
-
             toTerminal.RealizarTravessia(chefeDeServico);
-            toTerminal.Mensagens.ForEach(Console.WriteLine);
-
             toAviao.RealizarTravessia(chefeDeServico, piloto);
-            toAviao.Mensagens.ForEach(Console.WriteLine);
 
             Console.ReadKey();
         }
